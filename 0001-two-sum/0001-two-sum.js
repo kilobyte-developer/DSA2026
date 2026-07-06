db4c0b9c06b80ddb6b9948f91366ab1609ee1bf5
Atharva Funde <atharvafunde26@gmail.com>
2026-07-06 08:13:03 +0530

    Time: 35 ms (19.95%), Space: 56 MB (20.22%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let ans = []
+    let n = nums.length
+
+    for(let i = 0; i<n-1; i++){
+        for(let j = i+1; j<n; j++){
+            if(nums[i]+nums[j]==target){
+                ans.push(j)
+                ans.push(i)
+                break
+            }
+        }
+    }
+    return ans
+};
