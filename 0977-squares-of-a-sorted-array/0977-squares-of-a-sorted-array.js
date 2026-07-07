87e78a0f65771847c2068cbd39e03d3e44842bbc
Atharva Funde <atharvafunde26@gmail.com>
2026-07-07 12:44:46 +0530

    Time: 13 ms (13.1%), Space: 62.7 MB (35.08%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -6,10 +6,22 @@ var sortedSquares = function(nums) {
     let pt1 = 0
     let pt2 = nums.length-1
     let ans = []
+    let idx = nums.length -1
     
-    for(let i = 0; i<nums.length; i++){
-        ans.push(nums[i]*nums[i])
+    function sqr(n){
+        return n*n
     }
-    ans.sort((a,b)=>a-b)
+    while(pt1<=pt2){
+       if(Math.abs(nums[pt1])>Math.abs(nums[pt2])){
+            ans[idx] = Math.abs(sqr(nums[pt1]))
+            pt1++
+            idx--
+       }
+       else{
+        ans[idx] = Math.abs(sqr(nums[pt2]))
+        pt2--
+        idx--
+       }
+    }
     return ans
 };
